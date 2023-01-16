c492c432ca96026f072bf44d9c15faef77abaf67
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-16 11:18:55 +0000

    Create hello world route

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'pages#index'
+
+  get "/", to: "pages#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
